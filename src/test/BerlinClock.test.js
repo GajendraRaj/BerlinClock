615649806d3bff4d7122214fd77e5be9411ae377
GajendraRaj <Gajendra.rajput81@gmail.com>
2020-03-27 18:19:14 +0530

    test: Check BerlinClock component prop types

--- src/test/BerlinClock.test.js
+++ src/test/BerlinClock.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import BerlinClock from "../component/BerlinClock";
+import checkPropTypes from "check-prop-types";
 
 describe("BerlinClock component", () => {
   it("Should display Y in even second value", () => {
@@ -11,5 +12,33 @@ describe("BerlinClock component", () => {
   it("Should display O in odd second value", () => {
     const wrapper = shallow(<BerlinClock time={"15:33:13"} />);
     expect(wrapper.find("div").text()).toBe("O");
+  });
+
+  it("Should throw error message if there is no time prop", () => {
+    const errorMsg =
+      "Failed prop type: The prop `time` is marked as required in `<<anonymous>>`, but its value is `undefined`.";
+
+    const result = checkPropTypes(
+      BerlinClock.propTypes,
+      { time: undefined },
+      "prop",
+      BerlinClock.time
+    );
+
+    expect(result).toEqual(errorMsg);
+  });
+
+  it("Should throw error message if time prop type is not a string", () => {
+    const errorMsg =
+      "Failed prop type: Invalid prop `time` of type `number` supplied to `<<anonymous>>`, expected `string`.";
+
+    const result = checkPropTypes(
+      BerlinClock.propTypes,
+      { time: 12 },
+      "prop",
+      BerlinClock.time
+    );
+
+    expect(result).toEqual(errorMsg);
   });
 });
